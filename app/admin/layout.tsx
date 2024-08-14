a8479f003435481c9025c2d1a71385f86a3f150a
Erinle Samuel <psalmuelle1@gmail.com>
2024-08-14 22:42:53 +0100

    fix: restructure files and folders

---
+++ app/admin/layout.tsx
@@ -0,0 +1,48 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Layout } from "antd";
+import Image from "next/image";
+import Footer from "@/components/Footer";
+import "../globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "CHAIN WALLET",
+  description:
+    "Chain Wallet is a digital wallet designed for use with the TBDEX decentralized exchange. This wallet facilitates secure and private financial transactions, enabling users to trade and manage digital assets without relying on traditional intermediaries.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body className={inter.className + " max-w-[1440px] mx-auto"}>
+        <Layout>
+          <header className='bg-white flex justify-center items-center py-4 border-b'>
+            <Image
+              src={"/logo.png"}
+              width={36}
+              height={36}
+              alt={"Chain wallet"}
+            />
+            <p className='text-base font-mono font-bold tracking-widest'>
+              Chain Wallet - Admin Dashboard
+            </p>
+            <Image
+              src={"/logo.png"}
+              width={36}
+              height={36}
+              alt={"Chain wallet"}
+            />
+          </header>
+          <Layout>{children}</Layout>
+          <Footer />
+        </Layout>
+      </body>
+    </html>
+  );
+}
---
+++ app/admin/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Admin (){
+    return (
+        <main className="min-h-[80vh]">
+            This is the admin page that i want to build
+        </main>
+    )
+}
