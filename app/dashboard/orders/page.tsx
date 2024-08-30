a428c5859572ac8f4ba136dd249319b10fef9f21
Erinle Samuel <psalmuelle1@gmail.com>
2024-08-30 16:43:40 +0100

    feat: create order component

--- app/dashboard/orders/page.tsx
+++ app/dashboard/orders/page.tsx
@@ -1,20 +1,26 @@
 "use client";
 import Order from "@/components/orders/Order";
 import { Layout } from "antd";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import initWeb5 from "@/lib/web5/web5";
 
 const { Content } = Layout;
 
 export default function Orders() {
+  const [orders, setOrders] = useState([]);
 
-    useEffect(()=>{
-        console.log('Orders page loaded');
-    },[])
+  useEffect(() => {
+    //init web5
+    //Get All pfiDIDs from the dwn
+    //Get exchanges from those pfis
+    //Get all orders from the exchanges
+    // Save them to a useState
+  }, []);
   return (
     <Content className='mt-8 mx-4'>
       <h1 className='text-base font-bold mb-4'>Orders</h1>
-      <div>
-        <Order />
+      <div className='mt-12 bg-white py-4'>
+        <p className='mb-4 mx-4 font-medium'>August 22, 2024</p>
       </div>
     </Content>
   );
--- components/orders/Order.tsx
+++ components/orders/Order.tsx
@@ -1,18 +1,76 @@
-import { RetweetOutlined } from "@ant-design/icons";
-import { Avatar, Badge } from "antd";
+import {
+  FullscreenOutlined,
+  RetweetOutlined,
+  RightOutlined,
+} from "@ant-design/icons";
+import { Avatar, Badge, Button, Tag } from "antd";
 
+type OrderProps = {
+  status: string;
+  date: string;
+  exhangeId: string;
+  payinCurrency: string;
+  payinAmount: string;
+  payoutCurrency: string;
+  payoutAmount: string;
+};
 
-export default function Order () {
-    return (
+export default function Order({
+  status,
+  date,
+  exhangeId,
+  payinCurrency,
+  payinAmount,
+  payoutCurrency,
+  payoutAmount,
+}: OrderProps) {
+  const statusColor: { [key: string]: string } = {
+    success: "green",
+    pending: "yellow",
+    processing: "blue",
+    failed: "red",
+  };
+  return (
+    <div className='flex justify-between items-center py-2 px-4 border-y cursor-pointer transition-transform transform hover:scale-[1.009] hover:bg-neutral-50'>
+      <div className='w-fit flex gap-2.5 justify-center items-center'>
+        <Badge dot color={statusColor[status]}>
+          <Avatar icon={<RetweetOutlined rotate={90} />} />
+        </Badge>
         <div>
-            <div>
-                <Badge dot color="blue">
-                <Avatar icon={<RetweetOutlined rotate={90} />} />
-                </Badge>
-                <div>
-                    <p>Exchange </p>
-                </div>
-            </div>
+          <p className='font-semibold'>Exchange</p>
+          <p>{date}</p>
         </div>
-    )
+      </div>
+      <div>
+        <Tag>{exhangeId}</Tag>
+      </div>
+      <div className='flex justify-center items-center gap-4'>
+        <div className='w-fit flex gap-2.5 justify-center items-center'>
+          <Avatar
+            src={"https://api.dicebear.com/9.x/shapes/svg?seed=Dusty"}
+            alt='avatar'
+          />
+          <div>
+            <p className='font-medium'>{payinAmount}</p>
+            <p>{payinCurrency}</p>
+          </div>
+        </div>
+        <div>
+          <RightOutlined />
+        </div>
+        <div className='w-fit flex gap-2.5 justify-center items-center'>
+          <Avatar
+            src={"https://api.dicebear.com/9.x/shapes/svg?seed=Maggie"}
+            alt='avatar'
+          />
+          <div>
+            <p className='font-medium'>{payoutAmount}</p>
+            <p>{payoutCurrency}</p>
+          </div>
+        </div>
+      </div>
+      <Button size='small'>{status}</Button>
+      <FullscreenOutlined />
+    </div>
+  );
 }
