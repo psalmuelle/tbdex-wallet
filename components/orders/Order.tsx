f5d79622ef8aead490d67f4710173dc42dc31e5b
Erinle Samuel <psalmuelle1@gmail.com>
2024-09-01 14:30:56 +0100

    chore: order component ui review

--- components/orders/Order.tsx
+++ components/orders/Order.tsx
@@ -37,7 +37,7 @@ export default function Order({ order }: OrderProps) {
 
   const status = "pending";
   return (
-    <div className='flex justify-between items-center py-2 px-4 border-y cursor-pointer transition-transform transform hover:scale-[1.009] hover:bg-neutral-50'>
+    <div className='flex justify-between items-center py-2 px-4 border-y cursor-pointer transition-transform transform hover:scale-[1.005] hover:bg-neutral-50'>
       <div className='w-fit flex gap-2.5 justify-center items-center'>
         <Badge dot color={statusColor[status]}>
           <Avatar icon={<RetweetOutlined rotate={90} />} />
@@ -50,10 +50,13 @@ export default function Order({ order }: OrderProps) {
       <div>
         <Tag>{order[1].metadata.id}</Tag>
       </div>
-      <div className='flex justify-center items-center gap-4'>
+      <div className='max-w-[185px] flex justify-center items-center gap-4'>
         <div className='w-fit flex gap-2.5 justify-center items-center'>
           <Avatar
-            src={"https://api.dicebear.com/9.x/shapes/svg?seed=Dusty"}
+            size={"small"}
+            src={
+              "https://api.dicebear.com/9.x/thumbs/svg?seed=Molly&backgroundType=gradientLinear&shapeColor=0a5b83,1c799f,69d2e7,f1f4dc,f88c49,transparent"
+            }
             alt='avatar'
           />
           <div>
@@ -66,11 +69,14 @@ export default function Order({ order }: OrderProps) {
         </div>
         <div className='w-fit flex gap-2.5 justify-center items-center'>
           <Avatar
-            src={"https://api.dicebear.com/9.x/shapes/svg?seed=Maggie"}
+            size={"small"}
+            src={
+              "https://api.dicebear.com/9.x/thumbs/svg?seed=Whiskers&backgroundType=gradientLinear&shapeColor=0a5b83,1c799f,69d2e7,f1f4dc,f88c49,transparent"
+            }
             alt='avatar'
           />
           <div>
-            <p className='font-medium'>{orderData.payout.amount}</p>
+            <p className='font-medium'>To</p>
             <p>{orderData.payout.currencyCode}</p>
           </div>
         </div>
